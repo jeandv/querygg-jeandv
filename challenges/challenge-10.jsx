9da57f444f661be2d2cdf00deec9fbff7613bd3b
jeandv <jeanrdev@gmail.com>
2025-09-29 20:31:42 -0400

    challenge - 10

--- challenges/challenge-10.jsx
+++ challenges/challenge-10.jsx
@@ -14,3 +14,96 @@
 // Si todas las consultas son exitosas, deben devolverse las propiedades reviews y book.
 
 // Cada consulta debe tener su propia entrada de caché.
+
+//App.jsx
+import * as React from "react";
+import { useQueries } from "@tanstack/react-query";
+import { getBook, getReviewsForBook } from "./utils";
+import ReviewFormSection from "./ReviewFormSection";
+import ReviewsSection from "./ReviewsSection";
+import BookDetails from "./BookDetails";
+import { Error, Loading } from "./MessageComponents";
+
+function useBookDetails(bookId) {
+  const results = useQueries({
+    queries: [
+      {
+        queryKey: ["book", { bookId }],
+        queryFn: () => getBook(bookId),
+        enabled: !!bookId,
+      },
+      {
+        queryKey: ["reviews", { bookId }],
+        queryFn: () => getReviewsForBook(bookId),
+        enabled: !!bookId,
+      },
+    ],
+  });
+
+  const bookResult = results[0];
+  const reviewsResult = results[1];
+
+  const isPending = bookResult.isPending || reviewsResult.isPending;
+  const isError = bookResult.isError || reviewsResult.isError;
+  
+  return {
+    isPending,
+    isError,
+    book: bookResult.data,
+    reviews: reviewsResult.data,
+  };
+}
+
+function Book({ bookId }) {
+  const { isPending, isError, reviews, book } = useBookDetails(bookId);
+
+  if (isError) {
+    return <Error />;
+  }
+
+  if (isPending) {
+    return <Loading />;
+  }
+
+  return (
+    <main className="book-detail">
+      <div>
+        <span className="book-cover">
+          <img src={book?.thumbnail} alt={book?.title} />
+        </span>
+      </div>
+      <div>
+        <BookDetails book={book} />
+        <ReviewFormSection />
+        <ReviewsSection reviews={reviews} />
+      </div>
+    </main>
+  );
+}
+
+export default function App() {
+  const [selectedBookId, setSelectedBookId] = React.useState("pD6arNyKyi8C");
+
+  return (
+    <div>
+      <header className="app-header">
+        <h1>
+          <span>Query Library</span>
+        </h1>
+        <div className="select">
+          <select
+            value={selectedBookId}
+            onChange={(e) => setSelectedBookId(e.target.value)}
+          >
+            <option value="pD6arNyKyi8C">The Hobbit</option>
+            <option value="aWZzLPhY4o0C">The Fellowship Of The Ring</option>
+            <option value="12e8PJ2T7sQC">The Two Towers</option>
+            <option value="WZ0f_yUgc0UC">The Return Of The King</option>
+          </select>
+        </div>
+      </header>
+
+      <Book bookId={selectedBookId} />
+    </div>
+  );
+}
