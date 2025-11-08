ec62cd1c110ccd42348d0856f7cfa911391e844e
jeandv <jeanrdev@gmail.com>
2025-11-08 03:05:33 -0400

    challenge 12

--- challenges/challenge-12.jsx
+++ challenges/challenge-12.jsx
@@ -24,3 +24,86 @@
 // Muestra los datos de la página anterior mientras se carga la página siguiente.
 
 // Establece la opacidad del componente <BookList /> a .5 cuando los datos sean placeholder data (datos de marcador de posición).
+
+
+import * as React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { createStarString, getData } from "./utils";
+import { ErrorMessage, Loading } from "./MessageComponents";
+import Pagination from "./Pagination";
+
+const searchTerm = "The Lord of the Rings";
+function useSearch(pageNumber) {
+  return useQuery({
+    queryKey: ["search", { searchTerm, pageNumber}],
+    queryFn: () => getData(searchTerm, pageNumber),
+    placeholderData: (previousData) => previousData
+  });
+}
+
+function BookList({ books, isPlaceholderData }) {
+  return (
+    <ul style={{ transition: "all ease 300ms", opacity: isPlaceholderData ? ".5" : "1" }}>
+      {books.map((book) => {
+        return (
+          <li key={book.id}>
+            <span className="book-cover">
+              <img src={book.thumbnail} alt={book.title} />
+            </span>
+            <h3 className="book-title">{book.title}</h3>
+            <small className="book-author">{book.authors.join(", ")}</small>
+            <span className="book-rating">
+              {createStarString(book.averageRating)}
+            </span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function PaginatedBookList() {
+  const [page, setPage] = React.useState(1)
+  const { data, status, isPlaceholderData } = useSearch(page);
+
+  if (status === "pending") {
+    return <Loading />;
+  }
+
+  if (status === "error") {
+    return <ErrorMessage />;
+  }
+
+  return (
+    <section className="search-results book-grid">
+      <div>
+        <header>
+          <h2>
+            Search results for <strong>{searchTerm}</strong>
+          </h2>
+          <Pagination
+            totalPages={100}
+            activePage={page}
+            setActivePage={setPage}
+          />
+        </header>
+        <BookList books={data.books} isPlaceholderData={isPlaceholderData} />
+      </div>
+    </section>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <header className="app-header">
+        <h1>
+          <span>Query Library</span>
+        </h1>
+      </header>
+      <main>
+        <PaginatedBookList />
+      </main>
+    </div>
+  );
+}
