8f5f04f7240a4bd2d76f382866a8a9ff5a85e4f4
jeandv <jeanrdev@gmail.com>
2025-11-14 00:40:34 -0400

    challenge 18

---
+++ challenges/challenge-18.jsx
@@ -0,0 +1,11 @@
+// En este desafío, nuestro objetivo es refactorizar nuestra aplicación para usar un enfoque más estructurado para definir nuestras QueryKeys. Crearemos dos fábricas, bookQueries y reviewQueries, que nos ayudarán a definir nuestras QueryKeys de una manera más consistente. Para lograr esto, editarás tanto queries.js como mutations.js.
+
+// 🎯 Tareas a Realizar
+
+// Crear una fábrica de queries para bookQueries con keys para all, detail, featured, myBooks y search.
+
+// Crear y exportar una fábrica de queries para reviewQueries con keys para all, detail y latest.
+
+// Refactorizar todas las queries para usar las nuevas fábricas de queries.
+
+// Refactorizar todas las mutaciones para usar las nuevas fábricas de queries.
